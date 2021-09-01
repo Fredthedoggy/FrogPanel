9f63979b8bcacbb8d71a0edaaf0031b2c5e2f35d
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2021-08-31 21:58:38 -0400

    Add Folder Viewing While Editing File

--- resources/scripts/components/NavigationBar.tsx
+++ resources/scripts/components/NavigationBar.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCogs, faLayerGroup, faSignOutAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { useStoreState } from 'easy-peasy';
+import { ApplicationStore } from '@/state';
 import SearchContainer from '@/components/dashboard/search/SearchContainer';
 import tw, { theme } from 'twin.macro';
 import styled from 'styled-components/macro';
@@ -10,6 +13,14 @@ const Navigation = styled.div`
 
     & > div {
         ${tw`mx-auto w-full flex items-center`};
+    }
+
+    & #logo {
+        ${tw`flex-1`};
+
+        & > a {
+            ${tw`text-2xl font-header px-4 no-underline text-neutral-200 hover:text-neutral-100 transition-colors duration-150`};
+        }
     }
 `;
 
@@ -30,11 +41,30 @@ const RightNavigation = styled.div`
 `;
 
 export default () => {
+    const name = useStoreState((state: ApplicationStore) => state.settings.data!.name);
+    const rootAdmin = useStoreState((state: ApplicationStore) => state.user.data!.rootAdmin);
+
     return (
         <Navigation>
-            <div css={tw`mx-auto w-full flex items-center`} style={{ height: '3.5rem' }}>
-                <RightNavigation css={tw`ml-auto`}>
+            <div css={tw`mx-auto w-full flex items-center`} style={{ maxWidth: '1200px', height: '3.5rem' }}>
+                <div id={'logo'}>
+                    <Link to={'/'}>
+                        {name}
+                    </Link>
+                </div>
+                <RightNavigation>
                     <SearchContainer/>
+                    <NavLink to={'/'} exact>
+                        <FontAwesomeIcon icon={faLayerGroup}/>
+                    </NavLink>
+                    <NavLink to={'/account'}>
+                        <FontAwesomeIcon icon={faUserCircle}/>
+                    </NavLink>
+                    {rootAdmin &&
+                    <a href={'/admin'} rel={'noreferrer'}>
+                        <FontAwesomeIcon icon={faCogs}/>
+                    </a>
+                    }
                     <a href={'/auth/logout'}>
                         <FontAwesomeIcon icon={faSignOutAlt}/>
                     </a>
