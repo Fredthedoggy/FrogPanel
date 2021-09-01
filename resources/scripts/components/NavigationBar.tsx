24f558e5e56c17a2d82f222377a0b5c74a157088
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2021-08-31 21:58:38 -0400

    Add Folder Viewing While Editing File

--- resources/scripts/components/NavigationBar.tsx
+++ resources/scripts/components/NavigationBar.tsx
@@ -10,14 +10,14 @@ import styled from 'styled-components/macro';
 
 const Navigation = styled.div`
     ${tw`w-full bg-neutral-900 shadow-md overflow-x-auto`};
-    
+
     & > div {
         ${tw`mx-auto w-full flex items-center`};
     }
-    
+
     & #logo {
         ${tw`flex-1`};
-        
+
         & > a {
             ${tw`text-2xl font-header px-4 no-underline text-neutral-200 hover:text-neutral-100 transition-colors duration-150`};
         }
@@ -26,14 +26,14 @@ const Navigation = styled.div`
 
 const RightNavigation = styled.div`
     ${tw`flex h-full items-center justify-center`};
-    
+
     & > a, & > .navigation-link {
         ${tw`flex items-center h-full no-underline text-neutral-300 px-6 cursor-pointer transition-all duration-150`};
-        
+
         &:active, &:hover {
             ${tw`text-neutral-100 bg-black`};
         }
-        
+
         &:active, &:hover, &.active {
             box-shadow: inset 0 -2px ${theme`colors.cyan.700`.toString()};
         }
