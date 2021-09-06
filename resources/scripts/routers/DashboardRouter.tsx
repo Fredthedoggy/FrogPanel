d055b76a594a67ddb25eaca8a1948f1a2b633271
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2021-09-06 10:30:54 -0400

    Add SidePanel

--- resources/scripts/routers/DashboardRouter.tsx
+++ resources/scripts/routers/DashboardRouter.tsx
@@ -1,39 +1,46 @@
 import React from 'react';
-import { NavLink, Route, RouteComponentProps, Switch } from 'react-router-dom';
+import { Route, RouteComponentProps, Switch } from 'react-router-dom';
 import AccountOverviewContainer from '@/components/dashboard/AccountOverviewContainer';
 import NavigationBar from '@/components/NavigationBar';
 import DashboardContainer from '@/components/dashboard/DashboardContainer';
 import AccountApiContainer from '@/components/dashboard/AccountApiContainer';
 import { NotFound } from '@/components/elements/ScreenBlock';
 import TransitionRouter from '@/TransitionRouter';
-import SubNavigation from '@/components/elements/SubNavigation';
+import { CSSTransition } from 'react-transition-group';
+import SidePanel, { Category, Link } from '@/components/SidePanel';
+import tw from 'twin.macro';
+import { faCog, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
 
 export default ({ location }: RouteComponentProps) => (
-    <>
-        <NavigationBar/>
-        {location.pathname.startsWith('/account') &&
-        <SubNavigation>
-            <div>
-                <NavLink to={'/account'} exact>Settings</NavLink>
-                <NavLink to={'/account/api'}>API Credentials</NavLink>
-            </div>
-        </SubNavigation>
-        }
-        <TransitionRouter>
-            <Switch location={location}>
-                <Route path={'/'} exact>
-                    <DashboardContainer/>
-                </Route>
-                <Route path={'/account'} exact>
-                    <AccountOverviewContainer/>
-                </Route>
-                <Route path={'/account/api'} exact>
-                    <AccountApiContainer/>
-                </Route>
-                <Route path={'*'}>
-                    <NotFound/>
-                </Route>
-            </Switch>
-        </TransitionRouter>
-    </>
+    <div css={tw`flex flex-row`}>
+        <SidePanel>
+            {location.pathname.startsWith('/account') &&
+            <CSSTransition timeout={150} classNames={'fade'} appear in>
+                <Category name={'ACCOUNT'}>
+                    <Link name={'Settings'} icon={faCog} react link={'/account'} exact/>
+                    <Link name={'API Credentials'} icon={faProjectDiagram} react link={'/account/api'}/>
+                </Category>
+            </CSSTransition>
+            }
+        </SidePanel>
+        <div css={tw`flex-grow flex-shrink pl-56`}>
+            <NavigationBar/>
+            <TransitionRouter>
+                <Switch location={location}>
+                    <Route path={'/'} exact>
+                        <DashboardContainer/>
+                    </Route>
+                    <Route path={'/account'} exact>
+                        <AccountOverviewContainer/>
+                    </Route>
+                    <Route path={'/account/api'} exact>
+                        <AccountApiContainer/>
+                    </Route>
+                    <Route path={'*'}>
+                        <NotFound/>
+                    </Route>
+                </Switch>
+            </TransitionRouter>
+        </div>
+    </div>
 );
