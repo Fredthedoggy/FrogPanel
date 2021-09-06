7cea875847a7e96d8ae40ba76e0fad30fc4a19bc
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2021-09-06 10:30:54 -0400

    Add SidePanel

--- resources/scripts/components/NavigationBar.tsx
+++ resources/scripts/components/NavigationBar.tsx
@@ -1,67 +1,44 @@
 import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import SearchContainer from '@/components/dashboard/search/SearchContainer';
-import tw from 'twin.macro';
+import tw, { theme } from 'twin.macro';
 import styled from 'styled-components/macro';
-import { useEffect, useState } from 'react';
 
 const Navigation = styled.div`
-    ${tw`w-full bg-neutral-900 shadow-md overflow-x-auto overflow-visible`};
+    ${tw`w-full bg-neutral-900 shadow-md overflow-x-auto`};
 
     & > div {
         ${tw`mx-auto w-full flex items-center`};
     }
 `;
 
-const NavigationSection = styled.div`
+const RightNavigation = styled.div`
     ${tw`flex h-full items-center justify-center`};
 
     & > a, & > .navigation-link {
-        ${tw`flex items-center h-full no-underline text-neutral-300 px-6 cursor-pointer transition-all duration-150 border-secondary-400`};
+        ${tw`flex items-center h-full no-underline text-neutral-300 px-6 cursor-pointer transition-all duration-150`};
 
         &:active, &:hover {
             ${tw`text-neutral-100 bg-black`};
         }
 
         &:active, &:hover, &.active {
-            ${tw`border-b-2`};
+            box-shadow: inset 0 -2px ${theme`colors.cyan.700`.toString()};
         }
     }
 `;
 
-export default (props: { setPanelShown: React.Dispatch<React.SetStateAction<boolean>> }) => {
-    const [ windowSize, setWindowSize ] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
-    });
-    useEffect(() => {
-        function handleResize () {
-            setWindowSize({ width: window.innerWidth, height: window.innerHeight });
-        }
-
-        window.addEventListener('resize', () => setWindowSize({ width: window.innerWidth, height: window.innerHeight }));
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    const setPanelShown = props.setPanelShown;
-
+export default () => {
     return (
         <Navigation>
             <div css={tw`mx-auto w-full flex items-center`} style={{ height: '3.5rem' }}>
-                {windowSize.width < 768 && (
-                    <NavigationSection>
-                        <a onClick={() => setPanelShown(true)}>
-                            <FontAwesomeIcon icon={faBars}/>
-                        </a>
-                    </NavigationSection>
-                )}
-                <NavigationSection css={tw`ml-auto`}>
+                <RightNavigation css={tw`ml-auto`}>
                     <SearchContainer/>
                     <a href={'/auth/logout'}>
                         <FontAwesomeIcon icon={faSignOutAlt}/>
                     </a>
-                </NavigationSection>
+                </RightNavigation>
             </div>
         </Navigation>
     );
